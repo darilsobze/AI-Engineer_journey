45eb42e8b97abd9f05779bf4ec62549bfdfb2b6a
darilsobze <darilsobze13@icloud.com>
2025-11-27 09:17:17 +0100

    solution 2_var

---
+++ Basics/Exercise/2_variables/2_variables_solution.py
@@ -0,0 +1,12 @@
+# break = 5 invalid syntax , cuz break is a keyword
+# exo2
+birth_year = 2004
+current_year = 2025
+my_age = current_year - birth_year
+
+#exo3
+first_name = "Daril"
+middle_name = "Vergesse"
+last_name = "Sobze Soffack"
+print("my full name ist " + first_name + " " + middle_name + " " + last_name)
+# invalid variables names :  1record record-one record^one continue
